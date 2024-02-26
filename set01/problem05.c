b7a8460b5cbe3fd7578b0c1ec790d9f354556478
swayam234567899000 <142883483+swayam234567899000@users.noreply.github.com>
2024-02-26 20:42:57 +0530

    committed 2/26/2024, 8:42:56 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,43 @@
+#include<stdio.h>
+int input();
+int compare(int a,int b,int c);
+void output(int a,int b, int c,int largest);
+int main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+}
+int input()
+{
+    int x;
+    printf("Enter the value\n");
+    scanf("%d",&x);
+    return x;
+}
+int compare(int a, int b, int c)
+{
+    int largest;
+    if(a>b && a>c)
+    {
+        largest=a;
+    }
+    if(b>a && b>c)
+    {
+        largest=b;
+    }
+    else
+    {
+        largest=c;
+    }
+    return largest;
+}
+void output(int a,int b,int c,int largest)
+{
+    printf("Largest among %d,%d,%d is %d\n",a,b,c,largest);
+}
 
