e112434d2e67a4e24809797f9d98b5147a01bd56
swayam234567899000 <142883483+swayam234567899000@users.noreply.github.com>
2024-03-04 14:15:49 +0530

    committed 3/4/2024, 2:15:48 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,35 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-struct _complex
-{
-  float real;
-  float imaginary; 
-};
-typedef struct _complex Complex;
-Complex input_complex()
-{
-    Complex n;
-    printf("Enter the Real and imaginary part of the number: ");
-    scanf("%f %f",&n.real,&n.imaginary);
-    return n;
-}
-Complex add_complex(Complex a,Complex b)
-{
-    Complex sum;
-    sum.real=a.real+b.real;
-    sum.imaginary=a.imaginary+b.imaginary;
-    return sum;
-}
-void output(Complex a, Complex b, Complex sum)
-{
-    printf("The sum of %f+%fi and %.2f+%.2fi is %.2f+%.2fi",a.real,a.imaginary,b.real,b.imaginary,sum.real,sum.imaginary);
-}
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
+
 int main()
 {
-    Complex a,b,sum;
-    a=input_complex();
-    b=input_complex();
-    sum=add_complex(a,b);
-    output(a,b,sum);
-    return 0;
+    char string1[100], string2[100]; 
+    int result;
+
+    input_two_strings(string1, string2);
+    result = stringcompare(string1, string2);
+    output(string1, string2, result);
+
+    return 0; 
+}
+
+void input_two_strings(char *string1, char *string2)
+{
+    printf("Enter the first string\n");
+    scanf("%s", string1);
+    printf("Enter the second string\n");
+    scanf("%s", string2);
+}
+
+int stringcompare(char *string1, char *string2)
+{
+    int i, result = 0;
+
+    for (i = 0; string1[i] != '\0' || string2[i] != '\0'; i++)
+    {
+        if (string1[i] > string2[i])
+        {
+            result = 1;
+        }
+        else if (string1[i] < string2[i])
+        {
+            result = 2;
+        }
+    }
+
+    return result;
+}
+
+void output(char *string1, char *string2, int result)
+{
+    if (result == 1)
+    {
+        printf("The %s string is larger than %s string\n", string1, string2);
+    }
+    else if (result == 2)
+    {
+        printf("The %s string is larger than %s string\n", string2, string1);
+    }
+    else
+    {
+        printf("The two strings are equal\n");
+    }
 }
